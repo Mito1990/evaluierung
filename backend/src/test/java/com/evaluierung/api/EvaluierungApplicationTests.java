414ace14310d3a1b6606b1f5ea9b8d3b8c86b085
Michael Toetsches <mitoit00@hs-esslingen.de>
2025-10-06 12:14:47 +0200

    initial commit backend

---
+++ backend/src/test/java/com/evaluierung/api/EvaluierungApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evaluierung.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvaluierungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
