ae7c8ab417d51f64b4aa6c38df462a3155b43095
Michael Toetsches <mitoit00@hs-esslingen.de>
2025-10-07 13:32:54 +0200

    initial commit frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+import React, { useEffect, useState } from "react";
+
+const App: React.FC = () => {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>React + TypeScript + PrimeReact + Axios läuft!</h1>
+    </div>
+  );
+};
+export default App;
---
+++ frontend/webpack.dev.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.tsx",
+  output: {
+    filename: "bundledev.js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/evaluation/"
+  },
+  devServer: {
+    static: path.join(__dirname, "public"),
+    port: 8023,
+    historyApiFallback: true
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"]
+  },
+  module: {
+    rules: [
+      { test: /\.tsx?$/, use: "ts-loader", exclude: /node_modules/ },
+      { test: /\.s?css$/, use: ["style-loader", "css-loader", "sass-loader"] }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html"
+    })
+  ]
+};
