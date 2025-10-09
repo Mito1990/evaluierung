f469811ce61e316d88ea598b8bbabfede1ef3efd
Michael Toetsches <mitoit00@hs-esslingen.de>
2025-10-09 12:24:01 +0200

    feat(prod): add WebConfig for produktion with springboot

---
+++ backend/src/main/java/com/evaluierung/api/config/WebConfig.java
@@ -0,0 +1,22 @@
+package com.evaluierung.api.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+
+        registry.addViewController("/evaluation")
+                .setViewName("forward:/evaluation/index.html");
+
+        registry.addViewController("/evaluation/{path:[^\\.]*}")
+                .setViewName("forward:/evaluation/index.html");
+
+        registry.addViewController("/evaluation/{path:^(?!.*\\.).*}/**")
+                .setViewName("forward:/evaluation/index.html");
+    }
+}
